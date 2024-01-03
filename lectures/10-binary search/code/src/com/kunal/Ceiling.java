d99123da9e79dff83725c9b1876529e8a1bf64ff
vivek8001 <63398787+vivek8001@users.noreply.github.com>
2024-01-03 14:15:56 +0530

    Update Ceiling.java

--- lectures/10-binary search/code/src/com/kunal/Ceiling.java
+++ lectures/10-binary search/code/src/com/kunal/Ceiling.java
@@ -4,6 +4,7 @@ public class Ceiling {
 
     public static void main(String[] args) {
         int[] arr = {2, 3, 5, 9, 14, 16, 18};
+        //just checking
         int target = 15;
         int ans = ceiling(arr, target);
         System.out.println(ans);
